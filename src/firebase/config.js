fd74d2ae43fc0e113d81d017531489d69c664554
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-01-09 19:51:49 +0800

    setup Firebase and reuse context and hooks

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3LUG88tUc6LCRc7nl_t7G4ZXQUgZvvOg",
+  authDomain: "mydojo-33fc1.firebaseapp.com",
+  projectId: "mydojo-33fc1",
+  storageBucket: "mydojo-33fc1.appspot.com",
+  messagingSenderId: "157786891803",
+  appId: "1:157786891803:web:0b6e40e0b0746f16673abe",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,95 @@
+import { useState, useEffect, useReducer } from "react";
+import { projectFirestore, timestamp } from "../firebase/config";
+
+let initialState = {
+  document: null,
+  isPending: false,
+  error: null,
+  success: false,
+};
+
+const firestoreReudcer = (state, action) => {
+  switch (action.type) {
+    case "IS_PENDING":
+      return {
+        document: null,
+        isPending: true,
+        error: null,
+        success: false,
+      };
+    case "ADDED_DOCUMENT":
+      return {
+        document: action.payload,
+        isPending: false,
+        error: null,
+        success: true,
+      };
+    case "DELETED_DOCUMENT":
+      return {
+        document: null,
+        isPending: false,
+        error: null,
+        success: true,
+      };
+    case "ERROR":
+      return {
+        document: null,
+        isPending: false,
+        error: action.payload,
+        success: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export const useFirestore = (collection) => {
+  const [response, dispatch] = useReducer(firestoreReudcer, initialState);
+  const [isCancelled, setIsCancelled] = useState(false);
+
+  // collection ref
+  const ref = projectFirestore.collection(collection);
+
+  // only dispatch if not cancelled
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
+    }
+  };
+
+  // add a document
+  const addDocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      const createdAt = timestamp.fromDate(new Date());
+      const addedDocument = await ref.add({ ...doc, createdAt });
+      dispatchIfNotCancelled({
+        type: "ADDED_DOCUMENT",
+        payload: addedDocument,
+      });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message });
+    }
+  };
+
+  // delete a document
+  const deleteDocument = async (id) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      await ref.doc(id).delete();
+      dispatchIfNotCancelled({
+        type: "DELETED_DOCUMENT",
+      });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: "could not delete" });
+    }
+  };
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { addDocument, deleteDocument, response };
+};
