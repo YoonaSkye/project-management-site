5c7cdc93a416f09edc3c225e4bfb42bf71f8cf2f
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-01-10 19:34:19 +0800

    create signup page

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,6 +1,54 @@
+import { useState } from "react";
+
 // styles
-import styles from "./Signup.module.css";
+import styles from "./Signup.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thunmbnail, setThumbnail] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
+  return (
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Sign up</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Display Name:</span>
+        <input
+          type="text"
+          required
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Profile thumbnail:</span>
+        <input type="file" required />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  );
 }
