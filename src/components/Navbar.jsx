e59bbc89f3f550a907a278d54b0aebbf7ee00603
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-01-26 14:52:27 +0800

    conditional navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // styles && images
 import styles from "./Navbar.module.css";
@@ -7,6 +8,7 @@ import Temple from "../assets/temple.svg";
 
 export default function Navbar() {
   const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
@@ -15,24 +17,30 @@ export default function Navbar() {
           <img src={Temple} alt="dojo logo" />
           <span>The Dojo</span>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isPending && (
-            <button className="btn" onClick={logout}>
-              Logout
-            </button>
-          )}
-          {isPending && (
-            <button className="btn" disabled>
-              Logging out
-            </button>
-          )}
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            {!isPending && (
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            )}
+            {isPending && (
+              <button className="btn" disabled>
+                Logging out
+              </button>
+            )}
+          </li>
+        )}
       </ul>
     </nav>
   );
