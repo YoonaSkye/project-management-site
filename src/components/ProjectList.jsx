0ebd47c024c05227a809cf679547994a13070db5
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-02-06 13:16:20 +0800

    fetch projects from database

---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,13 @@
+// styles
+import "./ProjectList.css";
+
+export default function ProjectList({ projects }) {
+  return (
+    <div>
+      {projects.length === 0 && <p>No Projects Yet!</p>}
+      {projects.map((project) => (
+        <div key={project.id}>{project.name}</div>
+      ))}
+    </div>
+  );
+}
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,19 @@
 // styles
-import styles from "./Dashboard.module.css";
+import "./Dashboard.css";
+
+// components
+import ProjectList from "../../components/ProjectList";
+
+import { useCollection } from "../../hooks/useCollection";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection("projects");
+
+  return (
+    <div>
+      <div className="page-title">Dashboard</div>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 }
