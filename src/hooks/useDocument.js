fb563b5676705c9d98edd5f6684f131cfa79c8c0
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-02-06 14:13:53 +0800

    project details page

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { projectFirestore } from "../firebase/config";
 
 export const useDocument = (collection, id) => {
-  const [document, setDocument] = useState([]);
+  const [document, setDocument] = useState(null);
   const [error, setError] = useState(null);
 
   // realtime  document data
--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,6 +1,24 @@
 // styles
-import styles from "./Project.module.css";
+import "./Project.css";
+
+import { useDocument } from "../../hooks/useDocument";
+import { useParams } from "react-router-dom";
 
 export default function Project() {
-  return <div>Project</div>;
+  const { id } = useParams();
+  const { document, error } = useDocument("projects", id);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  return (
+    <div className="project-details">
+      <h1>{document.name}</h1>
+    </div>
+  );
 }
