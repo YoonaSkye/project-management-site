4f949762d161444cb9a0e34ea853233113f83422
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-03-03 20:17:23 +0800

    完善ProjectSummary组件，创建者可以删除它

--- src/pages/project/ProjectSummary.jsx
+++ src/pages/project/ProjectSummary.jsx
@@ -1,9 +1,22 @@
+import { useFirestore } from "../../hooks/useFirestore";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useHistory } from "react-router-dom";
+
 // styles
 import "./Project.css";
 
 import Avatar from "../../components/Avatar";
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore("projects");
+  const { user } = useAuthContext();
+  const history = useHistory();
+
+  const handleClick = () => {
+    deleteDocument(project.id);
+    history.push("/");
+  };
+
   return (
     <div>
       <div className="project-summary">
@@ -21,6 +34,11 @@ export default function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
+      {user.uid === project.createdBy.id && (
+        <button className="btn" onClick={handleClick}>
+          Mark as Complete
+        </button>
+      )}
     </div>
   );
 }
